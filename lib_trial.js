f58be6b69b255d75ce2556d11a568c6647ff6ab3
juanEODATASCIENCE <95677618+juanEODATASCIENCE@users.noreply.github.com>
2022-07-13 19:20:25 +0200

    Update lib_trial.js

--- lib_trial.js
+++ lib_trial.js
@@ -4,6 +4,8 @@ The script is adapted from https://code.earthengine.google.com/2bca4e5f36d5a2d64
 Credits to Gennadii Donchyts.
 */
 
+var oeel=require('users/OEEL/lib:loadAll')
+
 var NDVI = function(img){
   return image.expression('float(b("B4") - b("B3")) / (b("B4") + b("B3"))');
   
