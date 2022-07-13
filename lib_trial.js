f9c054b83f8e9c9b30f397a73954213a90b33a8a
juanEODATASCIENCE <95677618+juanEODATASCIENCE@users.noreply.github.com>
2022-07-13 19:07:49 +0200

    Create lib_trial.js
    Add NDVI function

---
+++ lib_trial.js
@@ -0,0 +1,12 @@
+/*
+A sample Earth Engine JavaScript library.
+The script is adapted from https://code.earthengine.google.com/2bca4e5f36d5a2d642475a98fa421fa9
+Credits to Gennadii Donchyts.
+*/
+
+var NDVI = function(img){
+  return image.expression('float(b("B4") - b("B3")) / (b("B4") + b("B3"))');
+  
+}
+
+exports.NDVI = NDVI;
